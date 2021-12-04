1bfe1f9966c3ff542b9b3d5552ecbb3a32838061
Vitor Aguiar <v.aguiar@hotmail.com>
2021-12-03 23:56:34 -0300

    comit

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,14 +1,38 @@
 import React from "react";
+import { Fontisto } from '@expo/vector-icons'
 import { Background } from "../../compoments/Background";
+import { BorderlessButton } from "react-native-gesture-handler";
+import { ImageBackground } from "react-native";
+import { styles } from "./styles";
 import { Header } from '../../compoments/Header'
+import { BannerImg } from '../../assets/banner.png'
+import { theme } from "../../global/styles/theme";
 
 export function AppointmentDetails() {
-  return(
+  return (
     <Background>
-      <Header 
+      <Header
         title="Detalhes"
+        action={
+          <BorderlessButton>
+            <Fontisto
+              name="share"
+              size={24}
+              color={theme.colors.primary}
+            />
+          </BorderlessButton>
+        }
       />
-      
+
+      <ImageBackground
+        source={BannerImg}
+        style={styles.banner}
+      >
+
+
+      </ImageBackground>
+
+
     </Background>
   )
 }
